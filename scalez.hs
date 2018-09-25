b117a17186242a846e01a08e34083a16625f8aa8
nkleemann <n.kleemann@posteo.de>
2018-09-25 23:44:16 +0200

    simplified scale model by removing it :)

--- scalez.hs
+++ scalez.hs
@@ -1,6 +1,14 @@
+-- Ideen:
+--      .midi Datei generieren
+--      Audio abspielen
+
 module SCALEZ where
 
 import qualified Data.Map as Map
+
+--
+--      DATA TYPES
+--
 
 data Note
     = C 
@@ -17,15 +25,14 @@ data Note
     | B deriving (Show) -- TODO own instance
 
 data Step
-    = Whole | Half
+    = Whole | Half deriving (Show)
 
-data Scale 
-    = Scale
-      {
-          steps :: [Step]
-      }
+type SemiTone  = Int
+type ScaleName = String
 
-type SemiTone = Int
+--
+--      CONSTANTS
+--
 
 notes :: Map.Map SemiTone Note
 notes 
@@ -44,18 +51,33 @@ notes
                        (12, B)
                    ]
 
-scalez :: Map.Map String Scale
+scalez :: Map.Map ScaleName [Step]
 scalez 
     = Map.fromList [
-                       ("Major", Scale {steps=[Whole, Whole, Half, Whole, Whole, Whole, Half]})
+                       ("Major", [Whole, Whole, Half, Whole, Whole, Whole, Half])
                    ]
 
-to_note :: SemiTone -> Maybe Note
-to_note tone = Map.lookup tone notes
+--
+--      FUNCTIONS - MUSIC
+--
 
 whole_step :: SemiTone -> SemiTone
 whole_step = (+ 2)
 
 half_step  :: SemiTone -> SemiTone
 half_step  = (+ 1)
+
+--
+--      FUNCTIONS - UTILS
+--
+
+lookup_note :: SemiTone -> Maybe Note
+lookup_note tone = Map.lookup tone notes
+
+lookup_steps :: ScaleName -> Maybe [Step]
+lookup_steps name = Map.lookup name scalez
+
+build_from_root :: Note -> ScaleName -> [Note]
+build_from_root root name = undefined
+
 
