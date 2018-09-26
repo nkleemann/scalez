831d3e5323a40153a0731aaf60d05b6ce2d6544c
nkleemann <n.kleemann@posteo.de>
2018-09-26 23:57:00 +0200

    adding more scales and transforming into modules. scratching the key lookup

--- scalez.hs
+++ scalez.hs
@@ -1,12 +1,6 @@
 -- Ideen:
---      .midi Datei generieren
---      Audio abspielen
---      Draw mini ASCII Piano Scale Chart
-
---
--- Für Oktavdarstellung -> [D,E,FSharp,G,A,B,CSharp,D] -> [(D,1),(E,1),(FSharp,1),(G,1),(A,1),(B,1),(CSharp,2),(D,2)]
--- Oder: Funktion, die die Tastatur zeichnet und die Liste durchgeht sieht zwischen zwei Noten von selbst ob sie eine Oktave
--- überspannen. So kann an dieser Stelle reagiert werden. Die Oktavsache kümmert ja eh nur die Zeichen - Funktion.
+--      Audio abspielen -> runCommand "play -n synth 0.2 sine 440 &> /dev/null" >> return ()
+--      midi 
 
 module SCALEZ where
 
@@ -29,14 +23,16 @@ data Note
     | GSharp
     | A
     | ASharp
-    | B deriving (Show, Enum, Eq) -- TODO own instance
+    | B deriving (Show, Enum, Eq)
 
 data Step
     = Whole 
-    | Half deriving (Show)
+    | Half 
+    | AugSec deriving (Show)
 
 type SemiTone  = Int
 type ScaleName = String
+type Scale     = (ScaleName, [Step])
 
 
 --
@@ -46,21 +42,28 @@ type ScaleName = String
 scalez :: Map.Map ScaleName [Step]
 scalez 
     = Map.fromList [
-                       ("Major", [Whole, Whole, Half, Whole, Whole, Whole, Half])
+                       ("Major",            [Whole, Whole, Half, Whole, Whole, Whole, Half]),
+                       ("Minor",            [Whole, Half, Whole, Whole, Half, Whole, Whole]),
+                       ("Harmonic Minor",   [Whole, Half, Whole, Whole, Half, AugSec, Half]),
+                       ("Melodic Minor",    [Whole, Half, Whole, Whole, Whole, Whole, Half]),
+                       ("Dorian Mode",     [Whole, Half, Whole, Whole, Whole, Half, Whole])
                    ]
-
 
 --
 --      FUNCTIONS - MUSIC
 --
 
+-- |Perform a half step.
+half_step :: SemiTone -> SemiTone
+half_step = (+ 1)
+
 -- |Perform a whole step.
 whole_step :: SemiTone -> SemiTone
 whole_step = (+ 2)
 
--- |Perform a half step.
-half_step :: SemiTone -> SemiTone
-half_step = (+ 1)
+-- |Perform both half and whole step to form an augmented second.
+aug_sec :: SemiTone -> SemiTone
+aug_sec = (+ 3)
 
 -- |Keep a Tone in one and the same octave.
 same_octave :: SemiTone -> SemiTone
@@ -71,33 +74,38 @@ same_octave = flip mod 12
 transpose :: Note -> Step -> Note
 transpose note step
     = case step of
-            Half  -> to_note . same_octave . half_step  $ to_tone note
-            Whole -> to_note . same_octave . whole_step $ to_tone note
+            Half   -> to_note . same_octave . half_step  $ to_tone note
+            Whole  -> to_note . same_octave . whole_step $ to_tone note
+            AugSec -> to_note . same_octave . aug_sec    $ to_tone note
 
 -- |Generate a scale beginning at a root note.
 gen_scale :: Note -> [Step] -> [Note] 
 gen_scale root steps
-    = scanl transpose root steps -- TODO Span octaves!
+    = scanl transpose root steps
 
 
 --
 --      FUNCTIONS - UTILS
 --
 
+-- |Used for transposition
 to_tone :: Note -> SemiTone
 to_tone = fromEnum
 
+-- |Transform SemiTone back into classical notation as 'Note'
 to_note :: SemiTone -> Note
 to_note = toEnum
 
+-- |Search for a list of steps (Scale) by name 
 lookup_steps :: ScaleName -> Maybe [Step]
 lookup_steps name = Map.lookup name scalez
-
 
 
 --      TESTING
 
 major =         [Whole, Whole, Half, Whole, Whole, Whole, Half]
 major_from_d =  [D,E,FSharp,G,A,B,CSharp,D]
-works =         major_from_d == gen_scale D major 
+works =         major_from_d == gen_scale D major
+
+
 
