89d0deddf12d9275298592084388d2fd213bf7ca
nkleemann <n.kleemann@posteo.de>
2018-09-27 02:12:49 +0200

    adding new scales

--- Scale.hs
+++ Scale.hs
@@ -29,7 +29,6 @@ minor
           [Whole, Half, Whole, Whole, Half, Whole, Whole]
       )
 
-harmonic_minor :: Scale
 harmonic_minor 
     = (
           "Harmonic Minor", 
@@ -43,9 +42,53 @@ melodic_minor
           [Whole, Half, Whole, Whole, Whole, Whole, Half]
       )
 
+-- | Named after the Dorian Greeks.
 dorian_mode :: Scale
 dorian_mode 
     = (
           "Dorian Mode", 
           [Whole, Half, Whole, Whole, Whole, Half, Whole]
       )
+
+-- | Ancient Greek Scale attributed to Sappho, the 7th-century-B.C. poet and musician.
+mixolydian_mode :: Scale
+mixolydian_mode 
+    = (
+          "Mixolydian Mode", 
+          [Whole, Whole, Half, Whole, Whole, Half, Whole]
+      )
+
+pentatonic_neutral :: Scale
+pentatonic_neutral
+    = (
+          "Pentatonic Neutral",
+          [Whole,AugSec,Whole,AugSec]  
+      )
+
+pentatonic_major :: Scale
+pentatonic_major
+    = (
+          "Pentatonic Major",
+          [Whole,AugSec,Whole,Whole,AugSec]
+      )
+
+pentatonic_minor :: Scale
+pentatonic_minor
+    = (
+          "Pentatonic Minor",
+          [AugSec,Whole,Whole,AugSec,Whole]  
+      )
+
+pentatonic_blues :: Scale
+pentatonic_blues
+    = (
+          "Pentatonic Blues",
+          [AugSec,Whole,Half,Half,AugSec]  
+      )
+
+
+
+
+
+
+
