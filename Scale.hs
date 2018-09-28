c166c620b0347c2ddce150bcef12fa9de1df1da5
nkleemann <n.kleemann@posteo.de>
2018-09-28 23:55:17 +0200

    adding more scales

--- Scale.hs
+++ Scale.hs
@@ -29,6 +29,7 @@ minor
           [Whole, Half, Whole, Whole, Half, Whole, Whole]
       )
 
+harmonic_minor :: Scale
 harmonic_minor 
     = (
           "Harmonic Minor", 
@@ -40,6 +41,13 @@ melodic_minor
     = (
           "Melodic Minor", 
           [Whole, Half, Whole, Whole, Whole, Whole, Half]
+      )
+
+romanian_minor :: Scale
+romanian_minor 
+    = (
+          "Romanian Minor", 
+          [Whole, Half, AugSec, Half, Whole, Half, Whole]
       )
 
 -- | Named after the Dorian Greeks.
@@ -58,32 +66,77 @@ mixolydian_mode
           [Whole, Whole, Half, Whole, Whole, Half, Whole]
       )
 
+-- | Named after the anciant kingdom of Phrygia in Anatolia.
+phrygian_mode :: Scale
+phrygian_mode 
+    = (
+          "Phrygian Mode", 
+          [Half, Whole, Whole, Whole, Half, Whole, Whole]
+      )
+
+-- | Named after the anciant kingdom of Lydia in Anatolia.
+lydian_mode :: Scale
+lydian_mode 
+    = (
+          "Lydian Mode", 
+          [Whole, Whole, Whole, Half, Whole, Whole, Half]
+      )
+
+-- | Locrian is the word used to describe the inhabitants of the ancient Greek regions of Locris.
+locrian_mode :: Scale
+locrian_mode 
+    = (
+          "Locrian Mode", 
+          [Half, Whole, Whole, Half, Whole, Whole, Whole]
+      )
+
+spanish_gypsy :: Scale
+spanish_gypsy
+    = (
+          "Spanish Gypsy", 
+          [Half, AugSec, Half, Whole, Half, Whole, Whole]
+      )
+
+blues :: Scale
+blues
+    = (
+          "Blues", 
+          [AugSec, Whole, Half, Half, AugSec, Whole]
+      )
+
+enigmatic :: Scale
+enigmatic
+    = (
+          "Enigmatic", 
+          [Half, AugSec, Whole, Whole, Whole, Half, Half]
+      )
+
 pentatonic_neutral :: Scale
 pentatonic_neutral
     = (
           "Pentatonic Neutral",
-          [Whole,AugSec,Whole,AugSec]  
+          [Whole, AugSec, Whole, AugSec]  
       )
 
 pentatonic_major :: Scale
 pentatonic_major
     = (
           "Pentatonic Major",
-          [Whole,AugSec,Whole,Whole,AugSec]
+          [Whole, AugSec ,Whole, Whole, AugSec]
       )
 
 pentatonic_minor :: Scale
 pentatonic_minor
     = (
           "Pentatonic Minor",
-          [AugSec,Whole,Whole,AugSec,Whole]  
+          [AugSec, Whole, Whole, AugSec, Whole]  
       )
 
 pentatonic_blues :: Scale
 pentatonic_blues
     = (
           "Pentatonic Blues",
-          [AugSec,Whole,Half,Half,AugSec]  
+          [AugSec, Whole, Half, Half, AugSec]  
       )
 
 
